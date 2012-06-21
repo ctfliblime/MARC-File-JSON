087fd78c1f0e756e57ca600825e9c788a549b3a9
Clay Fouts <cfouts@liblime.com>
2012-06-20 17:27:46 -0700

    Add support for use in MARC::Batch

--- lib/MARC/File/JSON.pm
+++ lib/MARC/File/JSON.pm
@@ -3,31 +3,51 @@ package MARC::File::JSON;
 use strict;
 use warnings;
 use JSON;
+use JSON::Streaming::Reader;
 use MARC::File::Generic;
 use MARC::Record;
+use MARC::File;
 
-# JSON -> MARC::Record
-sub encode {
-    my $json = shift;
-    return MARC::Record->new_from_generic( JSON::XS->new->utf8->decode($json) );
-}
+use vars qw( @ISA );
+push @ISA, 'MARC::File';
 
 # MARC::Record -> JSON
-sub decode {
+sub encode {
     my $record = shift;
     return JSON->new->utf8->encode( $record->as_generic );
+}
+
+# JSON -> MARC::Record
+sub decode {
+    my ($self, $data) = @_;
+
+    if ( !ref($data) ) {
+        $data = JSON->new->utf8->decode( $data );
+    }
+    return MARC::Record->new_from_generic( $data );
+}
+
+sub _next {
+    my $self = shift;
+    my $jsonr
+        = $self->{jsonr} //= JSON::Streaming::Reader->for_stream($self->{fh});
+    my $token = $jsonr->get_token;
+    if ($token->[0] eq 'start_array') {
+        $token = $jsonr->get_token;
+    }
+    return ($token->[0] eq 'end_array') ? undef : $jsonr->slurp;
 }
 
 ### Methods injected into MARC::Record
 
 sub MARC::Record::new_from_json {
     my ($class, $json) = @_;
-    return encode( $json );
+    return __PACKAGE__->decode( JSON->new->utf8->decode($json) );
 }
 
 sub MARC::Record::as_json {
     my $self = shift;
-    return decode( $self );
+    return encode( $self );
 }
 
 1;
