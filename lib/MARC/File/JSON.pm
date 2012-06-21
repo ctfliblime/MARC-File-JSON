e95aa3a97d049b3c667e764c82b4a584b92c8d36
Clay Fouts <cfouts@liblime.com>
2012-06-21 14:10:40 -0700

    Adding POD docs

--- lib/MARC/File/JSON.pm
+++ lib/MARC/File/JSON.pm
@@ -1,5 +1,7 @@
 package MARC::File::JSON;
 
+# ABSTRACT: read/write MARC data into JSON format
+
 use strict;
 use warnings;
 use JSON;
@@ -8,7 +10,8 @@ use MARC::Record::Generic;
 use MARC::Record;
 use MARC::File;
 
-use vars qw( @ISA );
+use vars qw( @ISA $VERSION );
+$VERSION = '0.001';
 push @ISA, 'MARC::File';
 
 # MARC::Record -> JSON
@@ -51,3 +54,98 @@ sub MARC::Record::as_json {
 }
 
 1;
+
+__END__
+=head1 NAME
+
+MARC::File::JSON - Convert between MARC::Record objects and JSON
+formatted data.
+
+=head1 SYNOPSIS
+
+ use MARC::File::JSON;
+
+ my $record = MARC::Record->new_from_json( $json );
+ $json = $record->as_json;
+
+=head1 DESCRIPTION
+
+This module provides routines for converting between MARC::Record objects
+and serialized JSON data formatted as:
+
+ {
+   "leader":"01109cam a2200349 a 4500",
+   "fields":[
+     "001",
+     "   89009461 //r92",
+     "005",
+     "19991006093052.0",
+     "010", {
+       "subfields":[
+         "a",
+         "89009461 //r92"
+       ],
+       "ind1":" ",
+       "ind2":" "
+     },
+     "020",
+     ...
+   ]
+ }
+
+=head1 INTERFACE
+
+MARC::Record::Generic injects two subroutines into the MARC::Record
+namespace. Additionally it inherits from MARC::File and includes
+encode(), decode, and _next() routines, making it compatible with
+MARC::Batch.
+
+=over
+
+=item *
+
+MARC::Record::new_from_json( $json )
+
+A package method of MARC::Record which applies the values contained
+in C<$json> to the object which it returns.
+
+=item *
+
+MARC::Record::as_json( )
+
+An instance method for a MARC::Record object. Returns the objects
+values as a string of JSON data.
+
+=item *
+
+encode( MARC::Record $record )
+
+Returns a JSON string representation of $record.
+
+=item *
+
+decode( $class, $json )
+
+Converts $json into an instance of MARC::Record, applying the
+transform in MARC::Record::Generic.
+
+=back
+
+=head1 SEE ALSO
+
+MARC::Record::Generic
+
+Format inspired by http://dilettantes.code4lib.org/blog/2010/09/a-proposal-to-serialize-marc-in-json
+
+=head1 AUTHOR
+
+Clay Fouts <cfouts@khephera.net>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright (c) 2012 PTFS/LibLime
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
